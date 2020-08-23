bee46bb4cdc4397c1417ffcc190f96767e7c6bd1
SLIPPERS <evansonoka05.j@gmail.com>
2020-08-23 15:31:22 +0300

    mvc

---
+++ SOLAR/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user-pc\documents\pythonprogs\projects\serverside_laurene\solar\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
