fb58e2a4f4ffe47a947c4bb1672dfbde99c53325
SLIPPERS <evansonoka05.j@gmail.com>
2020-09-04 15:08:27 +0300

    added data

--- SOLAR/coreApp/admin.py
+++ SOLAR/coreApp/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import SocDoc
 
-admin.site.register(SocDoc)
+#admin.site.register(SocDoc)
+@admin.register(SocDoc)
+class SocDocAdmin(admin.ModelAdmin):
+    list_display = ['stateOfCharge', 'depthOfCharge', 'timeStamp' ]
+    list_filter = ['stateOfCharge']
+    search_fields = ['timeStamp']
