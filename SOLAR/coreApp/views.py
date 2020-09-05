5de0319b6b1d44128984c370cc7ac9d2d08d8f52
onellvine <50703714+onellvine@users.noreply.github.com>
2020-09-05 22:28:05 +0300

    Update views.py
    added frontend rendering page for records pulled from db

--- SOLAR/coreApp/views.py
+++ SOLAR/coreApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from . import models
+def login(request):
+    return render(request, 'login.html', {})
 
 def home(request):
-    
-    return render(request, 'home.html',{})
+    records = models.SocDoc.objects.values()
+    return render(request, 'home.html', {"records": list(records)})
